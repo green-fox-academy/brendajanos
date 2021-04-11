9f8680a0f577f98d43e37505a2c7f5268c583628
Janos Brenda <brenda.janos@icloud.com>
2021-04-11 13:50:15 +0200

    Day05 Homework Fix

--- week-1/day-05/draw-pyramid.ts
+++ week-1/day-05/draw-pyramid.ts
@@ -4,7 +4,7 @@ export {}
 // Write a program that draws a
 // pyramid like this:
 //
-//    *
+//    *                        
 //   ***
 //  *****
 // *******
@@ -16,7 +16,7 @@ let lineCount: number = 4;
 
 for (let i = 1; i <= lineCount; i++) { 
       let stars = ""; 
-      for (let j = 1; j <= (2 * lineCount - 1); j++) { 
+      for (let j = 1; j <= (2 * lineCount); j++) { 
          (j >= lineCount + 1 - i && j <= lineCount - 1 + i) ? stars += "*" : stars += " "; 
        } 
       console.log(stars); 
