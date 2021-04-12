e1887aa25d88a6472aaca2ee031ef5c4785a5da6
Janos Brenda <brenda.janos@icloud.com>
2021-04-12 21:35:10 +0200

    Day02 Functions

---
+++ week-2/day-01/Doubling.ts
@@ -0,0 +1,12 @@
+// -  Create a variable named `baseNumber` and assign the value `123` to it
+// -  Create a function called `doubleNumber()` that doubles it's input parameter
+//    and returns the doubled value
+// -  Log the result of `doubleNumber(baseNumber)` to the console
+ 
+var baseNumber = 123;
+
+function doubleNumber(a:number){
+    a *= 2;
+    return a;
+}
+console.log(doubleNumber(baseNumber));
